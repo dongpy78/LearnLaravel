0f2a85fc98d865065a149b9e598ff0a81dcad287
Van Dong <vandong78py@gmail.com>
2024-12-01 10:31:42 +0700

    Edit Post

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,12 @@ Route::post('/logout', [UserController::class, "logout"])->middleware(MustBeLogg
 // Blog post related routes
 Route::get('/create-post', [PostController::class, "showCreateForm"])->middleware(MustBeLoggedIn::class);
 Route::post('/create-post', [PostController::class, "storeNewPost"])->middleware(MustBeLoggedIn::class);
-Route::get('/post/{post}', [PostController::class, "viewSinglePost"]);
-Route::delete('/post/{post}', [PostController::class, 'delete']);
+Route::get('/post/{post}', [PostController::class, "viewSinglePost"]); //! Lưu ý không được để khoảng trắng
+Route::delete('/post/{post}', [PostController::class, 'delete'])->middleware('can:delete,post');
+Route::get('/post/{post}/edit', [PostController::class, 'showEditForm'])->middleware('can:update,post');
+Route::put('/post/{post}', [PostController::class, 'actuallyUpdate'])->middleware('can:update,post');
+
+
+
 // Profile related routes
 Route::get('/profile/{user:username}', [UserController::class, 'profile']);
